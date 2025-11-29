ac0ca93127a2d55d5700a9e3d8599b6c0c6f9a3d
vzou <zouval2006@gmail.com>
2025-11-29 19:30:14 +0100

    update player

---
+++ src/Model/player/ComputerPlayer.java
@@ -0,0 +1,52 @@
+package Model.player;
+
+import Model.Coordinate;
+import Model.EntityType;
+import Model.Game.GameConfiguration;
+import Model.Map.Grid;
+import Model.Weapon.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class ComputerPlayer extends Player{
+    // ---------------------
+    // A BESOIN DE GRID POUR FRONCTIONNER CORRECTEMENT
+    // ---------------------
+
+    public ComputerPlayer(GameConfiguration config) {
+        super(config);
+        this.m_name = "Computer";
+    }
+
+    @Override
+    public void placeEntity(Map<EntityType, List<Coordinate>> entityPlacement) {
+
+    }
+
+    public Coordinate choseCoord(){
+        // choisis une coordonnée de manière aléatoire et la renvoie
+
+        Random rand = new Random();
+
+        int x = rand.nextInt(this.m_ownGrid.getSize()); // 0 à 9
+        int y = rand.nextInt(this.m_ownGrid.getSize()); // 0 à 9
+
+        while(!this.m_ownGrid.isAlreadyHit(x, y)){
+            x = rand.nextInt(this.m_ownGrid.getSize()); // 0 à 9
+            y = rand.nextInt(this.m_ownGrid.getSize()); // 0 à 9
+        }
+        return new Coordinate(x, y);
+    }
+
+    public Weapon choseWeapon(){
+        // choisis une arme de manière aléatoire parmi la liste d'arme disponible et la renvoie
+
+        Random rand = new Random();
+        int nb = rand.nextInt(100);
+        int index = nb % this.availableWeapons.size();
+        return this.availableWeapons.get(index);
+    }
+}
---
+++ src/Model/player/Player.java
@@ -0,0 +1,78 @@
+package Model.player;
+
+import Model.Coordinate;
+import Model.EntityType;
+import Model.Game.GameConfiguration;
+import Model.GridEntity;
+import Model.Map.Grid;
+import Model.Weapon.Weapon;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public abstract class Player {
+
+    // ---------------------
+    // A BESOIN DE GRID ET GAME_CONFIG POUR FRONCTIONNER CORRECTEMENT
+    // ---------------------
+
+    protected String m_name;
+    protected Grid m_ownGrid;
+    protected Grid m_shotGrid;
+    private Integer m_nbBoatRemaning;
+    protected List<Weapon> availableWeapons;
+    //private WeaponFactory WFacto;
+    // private  List<Trap> traps; TODO
+
+
+    public Player(GameConfiguration config) {
+        this.m_name = config.getNickName();
+        //this.m_ownGrid = new Grid(config.getGridSize(), config.getGridEntityPlacement());
+        this.m_shotGrid = new Grid(config.getGridSize());
+        this.availableWeapons = new ArrayList<>();
+        // this.m_nbBoatRemaning = A voir
+        // this.traps = new ArrayList<>(); TODO
+
+        this.placeEntity(config.getGridEntityPlacement());
+
+    }
+
+    public void placeEntity(Map<EntityType, List<Coordinate>> entityPlacement) {
+        this.m_ownGrid.placeEntity(entityPlacement);
+    }
+
+    public Grid getOwnGrid(){return this.m_ownGrid;}
+    public Grid getShotGrid(){return this.m_shotGrid;}
+    public Integer getGridSize(){return this.m_ownGrid.getSize();}
+
+    public void addWeapon(Weapon w){
+        availableWeapons.add(w);
+    }
+
+    public void markMiss(Coordinate coord){
+        this.m_ownGrid.markMiss(coord.getX(), coord.getY());
+    }
+
+    public GridEntity getGridEntityFromCoord(Coordinate coord){
+        return this.m_ownGrid.getEntityFromCoord(coord);
+    }
+
+    public void triggerTornado(Coordinate coord){
+        this.m_ownGrid.triggerTornado(coord);
+    }
+
+    public void loseOneBoat(){
+        this.m_nbBoatRemaning --;
+    }
+
+    public boolean hasLost(){
+        return this.m_nbBoatRemaning == 0;
+    }
+
+    public void receiveShot(Coordinate coord) {
+        this.m_ownGrid.hit(coord.getX(),coord.getY());
+    }
+
+
+}
